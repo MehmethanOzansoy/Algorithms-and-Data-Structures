cb6b76de3f8a0ee4920667ef480a1ff9d8f2736d
Mehmet Han Ozansoy <94033888+MehmethanOzansoy@users.noreply.github.com>
2023-04-29 17:47:10 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class Class1<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] a;
+
+        public int Capacity => a.Length;
+        public int Count { get; private set; }
+
+        public Class1() {
+            
+            a = new T[20];
+            Count = 0;
+        }
+
+        public Class1(params T[] initial)
+        {
+            a = new T[initial.Length];
+            Count++;
+            foreach (var item in initial)
+            {
+                Add(item);
+            }
+        }
+
+        public Class1(IEnumerable<T> collection)
+        {
+            a = new T[collection.ToArray().Length];
+            Count= 0;
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+
+        public void Add(T item)
+        {
+            if (a.Length == Count)
+                DoubleArray();
+
+
+            a[Count] = item;
+            Count++;
+        }
+
+        private void DoubleArray()
+        {
+           var temp = new T[a.Length *2];
+           for (int i = 0; i < a.Length; i++)
+            {
+                temp[i] = a[i];
+            }
+           a = temp;
+        }
+
+        public T Remove()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("There is no more item to be removed from the array");
+            }
+
+            if(a.Length/4 >= Count)
+            {
+                HalfArray();
+            }
+            
+            var temp = a[Count-1];
+
+            if(Count>0)
+                Count--;
+
+            return temp;
+        }
+
+        private void HalfArray()
+        {
+            if (a.Length > 2)
+            {
+                var temp = new T[a.Length/2];
+                System.Array.Copy(a, temp, temp.Length);          
+                a = temp;
+
+                /*
+                 *  for (int i=0; i<a.Length; i++)
+                {
+                    temp[i] = a[i];
+                }
+                 * Sytem.Array.Copy(a,temp,temp.Lenght)
+                 */
+            }
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return a.Take(Count).GetEnumerator();
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Dynamic;
+
+namespace DataStructures // Note: actual namespace depends on the project name.
+{
+    internal class Program:Class1<T>
+    {
+        static void Main(string[] args)
+        {
+            var p0 = new DataStructures.Class1<int>(1, 2, 3, 4, 5, 6, 7, 8);
+            var p1 = new int[] {8,8,9,10};
+            var p2 = new List<int>() { 5,10,15,20,25};
+            var p3 = new ArrayList() { 5, 10, 5, 53, 77 };
+
+            var arr = new DataStructures.Class1<int>(p1);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+
+}
+/*
+             *     var arrObj = new List<string>();
+                        arrObj.Add("a");
+                        arrObj.Add("b");
+                        arrObj.Add("c");
+
+                        foreach (var item in arrObj)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+            */
+
+/*
+var obj = Array.CreateInstance(typeof(int),5);
+
+obj.SetValue(155, 0);
+obj.SetValue(255, 1);
+obj.SetValue(199, 2);
+obj.SetValue(2, 3);
+obj.SetValue(3, 4);
+
+foreach (var item in obj)
+{
+   Console.WriteLine(item);
+}
+
+
+var arrInt = new List<int>();
+
+arrInt.Add(1);
+arrInt.Add(2);
+
+arrInt.AddRange(new int[] { 1, 2, 3 });
+arrInt.RemoveAt(4);
+
+foreach (int i in arrInt)
+{
+    Console.WriteLine(i);
+}
+
+arrInt.Clear();
+*/
+
+/*
+ *  Böyle yazma yerine yapıcının içinden çağırabiliriz
+    arr.Add(11);
+    arr.Add(21);
+    arr.Add(15);
+    arr.Add(24);
+     *  foreach (var item in arr)
+    {
+        Console.WriteLine(item);
+    }
+      arr.Where(x => x%2 == 1).ToList().ForEach(x => Console.WriteLine(x));
+
+    arr.Remove();
+
+    Console.WriteLine($" {arr.Count} /{arr.Capacity}");
+    Console.ReadLine();
+ */
+
